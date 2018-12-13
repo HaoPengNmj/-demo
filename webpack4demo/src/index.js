039b6f6218095da77b550310bfc4e421d06d519e
haopeng <2842382609@qq.com>
2018-12-13 23:07:19 +0800

    webpack4

--- webpack4demo/src/index.js
+++ webpack4demo/src/index.js
@@ -1,12 +1,24 @@
 import _ from 'lodash';
+import './style.css';
+import Icon from './pic.jpg';
 
 function component() {
-    var element = document.createElement('div');
-  
-    // Lodash（目前通过一个 script 脚本引入）对于执行这一行是必需的
-    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  
-    return element;
-  }
-  
-  document.body.appendChild(component());
+  var element = document.createElement('div');
+
+  // Lodash（目前通过一个 script 脚本引入）对于执行这一行是必需的
+  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+
+  return element;
+
+}
+
+function imgcomponent() {
+  // 将图像添加到我们现有的 div。
+  var myIcon = new Image();
+  myIcon.src = Icon;
+
+  return myIcon;
+}
+
+document.body.appendChild(component());
+document.body.appendChild(imgcomponent());
--- webpack4demo/webpack4.md
+++ webpack4demo/webpack4.md
@@ -16,4 +16,11 @@ https://www.cnblogs.com/imwtr/p/9189670.html
 ## 官方文档操作
 
 1. 无需config  直接npx webpack 
-2. 有config 文件  ，npx webpack --config webpack.config.js
+2. 有config 文件  ，npx webpack --config webpack.config.js 可以指定特定的配置文件，（改名）
+3. style css loader  转css
+4. css-loader 会解析css中的url，替换为`输 出`目录中图像的最终路径 
+5. css 分离用插件  
+6. 图片 字体 用 file-loader 
+7. 图片再优化    查看 [image-webpack-loader](https://github.com/tcoopman/image-webpack-loader) 和 [url-loader](https://www.webpackjs.com/loaders/url-loader)，以了解更多关于如果增强加载处理图片功能。 ？
+8. 属性resolve ？
+9. 
